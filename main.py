0bc6c486ecb75eb727a5aada349746826d44cde0
JUAN TAKO <juan.tako.dev@gmail.com>
2020-01-28 12:10:49 +0100

    tests

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 from miner import Miner
+import glob, os
+from multiprocessing.dummy import Pool as ThreadPool
 
+def mine_pdf(args):
+    (idx, m, file) = args
+    try:
+        json = m.pdf_to_json(pdf, ['pdf_main', 'pdf_address'])
+        print("[" + str(idx) + "] " + file + ": SUCCESS")
+    except Exception as e:
+        print("[" + str(idx) + "] " + file + ": FAILED")
+        print(e)
+
+
+pool = ThreadPool(4)
 
 miner = Miner()
 
-json = miner.pdf_to_json('data/fax_ser_8(31).pdf', ['pdf_main', 'pdf_address'])
-
-print(json)
+os.chdir("data")
+i = 0
+params = []
+for pdf in glob.glob("*.pdf"):
+    i+=1
+    params.append((i, miner, pdf))
+    
+pool.map(mine_pdf, params)
+#print(json)
