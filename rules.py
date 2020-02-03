e277ae9c46e746bb6cc3ed290b6ce4ad6ed79c79
JUAN-TAKO <juan.tako.dev@gmail.com>
2020-02-03 04:35:58 +0100

    more rules

--- rules.py
+++ rules.py
@@ -213,5 +213,129 @@ rules = {
                 once when p=5 and <<Cordialement>> weight 3 set p=6;
             """
         }
+    ],
+    "filassistance_header" : [
+        {
+            "min_score": 21,
+            "max_score": 29,
+            "rules": """
+                init f=0, d=0, c=0;
+
+                once after f=0 and <<\$Fax\ :>> set f=1;
+                once after f=1 and @$ set f=2;
+                once after f=2 and @$ set f=3;
+                start after f=3
+                stop when @$ store as "Assurance" weight 0;
+
+                start after <<Numéro de dossier \:\$>>
+                stop when @$ store as "Ref" weight 5;
+
+                once when d=0 and <<Planning d'intervention>> set d=1;
+                once when d=1 and <<pour le>> set d=2;
+
+                start when d=2 and @date
+                stop after @date set d=3
+                store as "Date" weight 5;
+
+                start after <<Bénéficiaire \:\$>>
+                stop when @$ store as "Dossier" weight 5;
+
+                start after <<mission suivante \:\$>>
+                stop when @$ store as "Type" weight 5;
+
+                once after c=0 and <<Total\$MISSION\$>> set c=1;
+                once after c=1 and @$ set c=2;
+
+                start after c=1
+                stop when @$ set c=2
+                store as "Charges" weight 5;
+
+                start after <<Téléphone (principal) \:>>
+                stop when @$ store as "Tél ev" weight 2;
+
+                start after <<Observations \:\$>>
+                stop when <<RECAPITULATIF>>
+                store as "Observations" weight 2; 
+                
+            """
+        },
+        {
+            "min_score": 18,
+            "max_score": 22,
+            "rules": """
+                init d=0, f=0;
+
+                once after f=0 and <<\$Fax\ :>> set f=1;
+                once after f=1 and @$ set f=2;
+                once after f=2 and @$ set f=3;
+                start after f=3
+                stop when @$ store as "Assurance" weight 0;
+
+                start after <<Numéro de dossier \:\$>>
+                stop when @$ store as "Ref" weight 5;
+
+                once when d=0 and <<Planning d'intervention>> set d=1;
+                once when d=1 and <<pour le>> set d=2;
+
+                start when d=2 and @date
+                stop after @date set d=3
+                store as "Date" weight 5;
+
+                start after <<Bénéficiaire \:\$>>
+                stop when @$ store as "Dossier" weight 5;
+
+                start after <<mission suivante \:\$>>
+                stop when @$ store as "Type" weight 5;
+                
+                start after <<Téléphone (principal) \:>>
+                stop when @$ store as "Tél ev" weight 2;
+
+            """
+        }
+    ],
+    "filassistance_address": [
+        {
+            "min_score": 12,
+            "max_score": 12,
+            "rules": """
+                init p=0;
+
+                once after <<Bénéficiaire \:\$>> set p=1;
+                once after @$ set p=2
+
+                start when p=2 stop after <<\, >> set p=3;
+                store as "Adresse 1" weight 3;
+
+                start when p=3 stop after @$ set p=4;
+                store as "Adresse 2" weight 3;
+
+                start when p=4 and @code
+                stop after @code set p=5
+                store as "Code" weight 3;
+
+                start when p=5 stop after @$ set p=6
+                store as "Ville" weight 3;
+            """
+        },
+        {
+            "min_score": 9,
+            "max_score": 9,
+            "rules": """
+                init p=0;
+
+                once after <<Bénéficiaire \:\$>> set p=1;
+                once after @$ set p=2
+
+                start when p=2 stop after @$ set p=3;
+                store as "Adresse 1" weight 3;
+
+                start when p=3 and @code
+                stop after @code set p=4
+                store as "Code" weight 3;
+
+                start when p=4 stop after @$ set p=5
+                store as "Ville" weight 3;
+            """
+        }
     ]
 }
